f82e20d464e29de9ddc29914c98d384242cc53cb
jfk@jolira.com <jfk@jolira.com@c86e12c4-a0bc-11de-afa9-e300858c10cc>
2011-04-05 04:21:38 +0000

    now storring the providers map in the HTTP session instead of the wicket session.
    git-svn-id: http://jolira-tools.googlecode.com/svn/wicket-guicier/trunk@3097 c86e12c4-a0bc-11de-afa9-e300858c10cc

--- guicier/src/main/java/com/google/code/joliratools/guicier/GuicierWebSession.java
+++ guicier/src/main/java/com/google/code/joliratools/guicier/GuicierWebSession.java
@@ -1,6 +1,8 @@
 /**
- * Copyright (c) 2010 jolira. All rights reserved. This program and the accompanying materials are made available under
- * the terms of the GNU Public License 2.0 which is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Copyright (c) 2010 jolira. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the GNU Public
+ * License 2.0 which is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 package com.google.code.joliratools.guicier;
@@ -8,8 +10,14 @@ package com.google.code.joliratools.guicier;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.apache.wicket.Request;
+import org.apache.wicket.RequestCycle;
 import org.apache.wicket.Session;
+import org.apache.wicket.protocol.http.WebRequest;
+import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.apache.wicket.protocol.http.WebSession;
 
 import com.google.inject.Key;
@@ -22,6 +30,7 @@ import com.google.inject.Scope;
  * @since 1.0
  */
 public class GuicierWebSession extends WebSession {
+    private static final String GUICIER_CHACHED_PROVIDERS = "guicierChachedProviders";
     private static final long serialVersionUID = 7170912849536454751L;
 
     /**
@@ -33,7 +42,25 @@ public class GuicierWebSession extends WebSession {
         return (GuicierWebSession) Session.get();
     }
 
-    private final Map<Key<?>, Provider<?>> cachedProviders = new HashMap<Key<?>, Provider<?>>();
+    private static Map<Key<?>, Provider<?>> getCachedProviders() {
+        final WebRequestCycle requestCycle = (WebRequestCycle) RequestCycle.get();
+        final WebRequest webRequest = requestCycle.getWebRequest();
+        final HttpServletRequest httpServletRequest = webRequest.getHttpServletRequest();
+        final HttpSession session = httpServletRequest.getSession();
+
+        @SuppressWarnings("unchecked")
+        Map<Key<?>, Provider<?>> cachedProviders = (Map<Key<?>, Provider<?>>) session
+                .getAttribute(GUICIER_CHACHED_PROVIDERS);
+
+        if (cachedProviders != null) {
+            return cachedProviders;
+        }
+
+        cachedProviders = new HashMap<Key<?>, Provider<?>>();
+        session.setAttribute(GUICIER_CHACHED_PROVIDERS, cachedProviders);
+
+        return cachedProviders;
+    }
 
     /**
      * Create a new session. Implementation just calls the super-class.
@@ -58,6 +85,8 @@ public class GuicierWebSession extends WebSession {
      * @see Scope#scope(Key, Provider)
      */
     protected synchronized <T> Provider<T> scope(final Key<T> key, final Provider<T> creator) {
+        final Map<Key<?>, Provider<?>> cachedProviders = getCachedProviders();
+
         @SuppressWarnings("unchecked")
         Provider<T> provider = (Provider<T>) cachedProviders.get(key);
 
