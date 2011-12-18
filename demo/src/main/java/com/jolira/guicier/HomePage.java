313be5bfd2d680fc589379d9d5550eeec9ddc6fd
Joachim Kainz <jfk@jolira.com>
2011-12-17 23:33:09 -0800

    ported to 1.5.

--- demo/src/main/java/com/jolira/guicier/HomePage.java
+++ demo/src/main/java/com/jolira/guicier/HomePage.java
@@ -1,13 +1,16 @@
 package com.jolira.guicier;
 
-import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 
 /**
  * For testing only
  */
 public class HomePage extends WebPage {
+    private static final long serialVersionUID = -7592981219887496438L;
+
     HomePage() {
         final PageParameters params = new PageParameters();
 
